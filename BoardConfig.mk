decf66f014add97024516fc677d5da37433e5f02
beanstown106 <nbenis106@gmail.com>
2015-08-07 05:13:31 -0400

    Build Kernel inline
    Change-Id: I03a9d9ce0cea87b5858d1a6318ab71cad45eefc2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,13 @@ ifneq ($(filter hammerhead_fp aosp_hammerhead_fp,$(TARGET_PRODUCT)),)
 BOARD_SEPOLICY_DIRS += \
        device/lge/hammerhead/sepolicy-hammerhead_fp
 
+# Define kernel config for inline building
+TARGET_KERNEL_CONFIG := hammerhead_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+
 # The list below is order dependent
 BOARD_SEPOLICY_UNION += \
        device.te \
